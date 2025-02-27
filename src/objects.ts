eec12732466c38f47d2c3fee41530f839a92f3d1
okoryem <okoryem@udel.edu>
2025-02-27 15:30:46 -0500

    renameQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -82,7 +82,9 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    let ques: Question = {...question};
+    ques.name = newName;
+    return {...ques};
 }
 
 /**
